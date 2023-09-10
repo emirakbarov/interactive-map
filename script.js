269a832092040319166f48bd5d1a380a0eb79a50
Emir Akbarov <akbarovemir3@gmail.com>
2023-09-10 22:09:25 +0100

    minor adjustments

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-checkWindowSize();
 
+#let popup = document.querySelector("#screenTooSmallPrompt");
+let elementsDisabled = document.querySelector("#disableOtherElements");
+let popupUp = false;
 
 function checkWindowSize() {
-    if (window.innterWidth < 340) {
-        
+    if (window.innerWidth < 580) {
+        elementsDisabled.style.display = "block";
+        popup.style.display = "block";
+        popupUp = true;
+    }
+    else {
+        elementsDisabled.style.display = "none";
+        popup.style.display = "none";
     }
 }
+
+window.addEventListener("resize", checkWindowSize);
+checkWindowSize();
